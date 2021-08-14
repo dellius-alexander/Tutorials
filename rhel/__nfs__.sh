45b6648d9a3b92c13fee57d69d74c1cbb9c7bc77
dellius-alexander <dellius.alexander@gmail.com>
2021-08-14 06:20:11 -0400

    added comments

--- rhel/__nfs__.sh
+++ rhel/__nfs__.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m' # Red
 NC='\033[0m' # No Color CAP
 ##########################################################################
-# Require to run script
+# REGEX patters
 # ip_address="(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 # _url_pattern="[((^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$)?|^[http|https|//|\w]{1}([\.][\w])*$]"
 # _doman_pattern="[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
@@ -23,39 +23,48 @@ fi
 #
 ##########################################################################
 ##########################################################################
+# mounts the nfs share
 __nfs__() {
-_input=($(echo ${@}))
-declare -p _input &>/dev/null
-if [[ $UID == 0 ]] &&  [[ ${#_input[@]} == 3 ]] && [[ ${_input[0]} =~ $_url_pattern ]] && [[ ${_input[1]} =~ $_file_pattern ]] && [[ ${_input[2]} =~ $_file_pattern ]]; then
-yum install -y nfs-utils firewalld
-wait $!
-printf "\n\n"
-[ ! -d "/mnt/nfs" ] && mkdir -p ${_input[2]}   # /mnt/local/nfs
-if [[ $(mount | grep -i 'nfs' | grep -i ${_input[0]} | grep -i ${_input[1]} | grep -ic ${_input[2]}) == 0 ]]; then
-    mount -t nfs -o nfsvers=4  ${_input[0]}:${_input[1]} ${_input[2]}
-    wait $!
-fi
-printf "\n\n"
-mount | grep nfs
-sleep 2
-wait $!
-printf "\n\n"
-if [[ $(cat /etc/fstab | grep -i ${_input[0]} | grep -i ${_input[1]} | grep -c ${_input[2]}) -eq 0 ]]; then
-cat >>/etc/fstab<<EOF 
-# Mount point for NFS Share
-${_input[0]}:${_input[1]} ${_input[2]}  nfs4     _netdev,auto,nosuid,rw,sync,hard,intr    0   0
+    # get user input
+    _input=($(echo ${@}))
+    declare -p _input &>/dev/null   # set variable
+    # validating the input values and positional parameters
+    if [[ $UID == 0 ]] &&  [[ ${#_input[@]} == 3 ]] && [[ ${_input[0]} =~ $_url_pattern ]] && [[ ${_input[1]} =~ $_file_pattern ]] && [[ ${_input[2]} =~ $_file_pattern ]]; then
+        # install packages for managing nfs 
+        yum install -y nfs-utils firewalld
+        wait $!
+        printf "\n\n"
+        # create the nfs directory if not exist
+        [ ! -d "/mnt/nfs" ] && mkdir -p ${_input[2]}   # /mnt/local/nfs
+        # deep check for the nfs matching input parameters
+        if [[ $(mount | grep -i 'nfs' | grep -i ${_input[0]} | grep -i ${_input[1]} | grep -ic ${_input[2]}) == 0 ]]; then
+            # mount nfs if no match is found
+            mount -t nfs -o nfsvers=4  ${_input[0]}:${_input[1]} ${_input[2]}
+            wait $!
+        fi
+        printf "\n\n"
+        # print mounted nfs info
+        mount | grep nfs
+        sleep 2
+        wait $!
+        printf "\n\n"
+        # check and update /etc/fstab to persit reboots, if necessary
+        if [[ $(cat /etc/fstab | grep -i ${_input[0]} | grep -i ${_input[1]} | grep -c ${_input[2]}) -eq 0 ]]; then
+        cat >>/etc/fstab<<EOF 
+        # Mount point for NFS Share
+        ${_input[0]}:${_input[1]} ${_input[2]}  nfs4     _netdev,auto,nosuid,rw,sync,hard,intr    0   0
 EOF
-printf "\n$(cat /etc/fstab | grep -i 'nfs')\n\n"
-printf "\nMounted ${_input[1]} successfully to ${_input[3]} from host ${_input[0]}...\n\n\n"
-ls -lia ${_input[2]}
-printf "\n\n"
-fi
-#
-else
-    printf "\nUsage: ${RED}sudo ${0} ${msgg} ${NC}\n"
-    printf "\nExample: ${RED}sudo ${0} 10.0.0.10 /nfsfileshare /mnt/nfsfileshare${NC}\n"
-#
-fi
-exit 0
+        printf "\n$(cat /etc/fstab | grep -i 'nfs')\n\n"
+        printf "\nMounted ${_input[1]} successfully to ${_input[3]} from host ${_input[0]}...\n\n\n"
+        ls -lia ${_input[2]}
+        printf "\n\n"
+        fi
+    
+    else
+    # Print error message on check failure
+        printf "\nUsage: ${RED}sudo ${0} ${msgg} ${NC}\n"
+        printf "\nExample: ${RED}sudo ${0} 10.0.0.10 /nfsfileshare /mnt/nfsfileshare${NC}\n"
+    fi
+    exit 0
 }
 __nfs__ $1 $2 $3
