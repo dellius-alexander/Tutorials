003ecd221d0fa86963c4423a5e945f7380ee045c
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2023-05-18 17:01:19 -0400

    Create healthcheck.js

---
+++ db/vector/.devcontainer/healthcheck/healthcheck.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const http = require("http");
+
+const options = {
+    host : process.env.HOSTNAME || 'localhost',
+    port : process.env.PORT || 3000,
+    timeout : 2000
+};
+
+const request = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    if (res.statusCode == 200) {
+        process.exit(0);
+    }
+    else {
+        process.exit(1);
+    }
+});
+
+request.on('error', function(err) {
+    console.log('ERROR');
+    process.exit(1);
+});
+
+request.end();
