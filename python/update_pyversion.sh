d969b2b3dcf66f33108427e9ef319131016f0bba
dellius-alexander <dellius.alexander@gmail.com>
2024-10-02 22:58:10 -0400

    added multi-python-versioning script

--- python/update_pyversion.sh
+++ python/update_pyversion.sh
@@ -9,22 +9,22 @@ fi
 
 # Install the latest version of Python: 3.10, 3.11, and 3.12
 # and virtual environment also
-yum update -y && yum install -y python3.9 python3.11 python3.12 \
-python3-pip python3.11-pip python3.12-pip
-
+yum update -y && yum install -y \
+python3.9 python3.11 python3.12 \
+python3-pip python3.11-pip python3.12-pip  1>&2 2>/dev/null
 
 # Update the alternatives for python3
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 3
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1 1>&2 2>/dev/null
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2 1>&2 2>/dev/null
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 3 1>&2 2>/dev/null
 
 # Update the alternatives for pip3
-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.9 1
-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.11 2
-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.12 3
+alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.9 1 1>&2 2>/dev/null
+alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.11 2 1>&2 2>/dev/null
+alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.12 3 1>&2 2>/dev/null
 
 # Set the default version of python3 to 3.12
-alternatives --set python3 /usr/bin/python3.12
-alternatives --set pip3 /usr/bin/pip3.12
+alternatives --set python3 /usr/bin/python3.12 1>&2 2>/dev/null
+alternatives --set pip3 /usr/bin/pip3.12 1>&2 2>/dev/null
 
-
+echo "Python 3.12 is now the default version"
